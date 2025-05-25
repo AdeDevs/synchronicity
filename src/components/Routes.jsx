b3f961c162ceab8b0c7607dfd8abd07454097d46
AdeDevs <adeyemiakinyemi01@gmail.com>
2025-05-25 21:39:27 +0100

    updt

--- src/components/Routes.jsx
+++ src/components/Routes.jsx
@@ -1,7 +1,38 @@
+import { useState } from "react"
+import { NavLink, useLocation } from "react-router-dom"
+
 export default function NavBar() {
+    const location = useLocation();
+
+    const pathClass = location.pathname.replace('/', '') || 'home';
+    const [showMenu, setShowMenu] = useState(false)
+    const toggleMenu = () => {
+        setShowMenu(!showMenu)
+    }
     return (
-        <div>
-            <h1>nav</h1>
+        <div className={`nav-bar ${pathClass}`}>
+            <ul className="desk-nav">
+                <li><NavLink to="/services">services</NavLink></li>
+                <li><NavLink to="/gallery">gallery</NavLink></li>
+                <li><NavLink to="/information">information </NavLink> </li>
+                <li><NavLink to="/schedule">schedule a shoot</NavLink></li>
+            </ul>
+            <h1 className="logo">A</h1>
+            <ul className="desk-nav">
+                <li><a href="#">contact us</a></li>
+                <li><a href="#">en</a></li>
+                <button>let's collaborate</button>
+            </ul>
+            <div className={`toggle ${showMenu ? "show" : ""}`} onClick={toggleMenu}>
+                <span className={`first ${pathClass}`}></span>
+                <span className={`second ${pathClass}`}></span>
+            </div>
+            <ul className={`mobile-nav ${showMenu ? "show" : ""}`}>
+                <li onClick={toggleMenu}><NavLink to="/services">services</NavLink></li>
+                <li onClick={toggleMenu}><NavLink to="/gallery">gallery</NavLink></li>
+                <li onClick={toggleMenu}><NavLink to="/information">information </NavLink> </li>
+                <li onClick={toggleMenu}><NavLink to="/schedule">schedule a shoot</NavLink></li>
+            </ul>
         </div>
     )
 }
